999428a92df40fec8d68314ed7213f5ea2ac5a4c
Leonardo Romeiro <leozinwork@gmail.com>
2024-01-22 17:48:38 -0300

    Adicionando construtor e melhorando class student.

--- PaymentContext.Domain/Entities/Student.cs
+++ PaymentContext.Domain/Entities/Student.cs
@@ -2,9 +2,28 @@ namespace PaymentContext.Domain.Entities;
 
 public class Student
 {
+    private List<Subscription> _subscriptions;
+    public Student(string firstName, string lastName, string document, string email)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Document = document;
+        Email = email;
+        _subscriptions=new();
+    }
+
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Document { get; set; }
     public string Email { get; set; }
-    public List<Subscription> Subscriptions { get; set; }
+    public string Address {get; set;}
+    public IReadOnlyCollection<Subscription> Subscriptions { get{ return _subscriptions.ToArray(); } }
+
+    public void AddSubscription(Subscription subscription){
+        foreach(var sub in Subscriptions){
+            sub.Active = false;
+        }
+
+        _subscriptions.Add(subscription);
+    }
 }
---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests
+{
+    [TestClass]
+    public class StudentTests{
+        [TestMethod]
+        public void TestMethod1(){
+            var student = new Student("Leonardo","Romeiro","14352611948","Leozinprogramador@gmail.com");
+        }
+    }
+}
+
