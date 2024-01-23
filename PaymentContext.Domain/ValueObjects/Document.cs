13ee707ecc54435e5e8fc64efcd55cd40cecae04
Leonardo Romeiro <leozinwork@gmail.com>
2024-01-23 11:18:36 -0300

    Enumerando documento

---
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -0,0 +1,14 @@
+using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Domain.ValueObjects;
+
+public class Document{
+    public Document(string number, EDocumentType type)
+    {
+        Number = number;
+        Type = type;
+    }
+    public string Number { get; private set; }
+    public EDocumentType Type { get; private set; }
+}
+
