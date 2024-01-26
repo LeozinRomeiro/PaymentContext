106ec6cdafc24c3a5fbc59172695c7262d5f1a9d
Leonardo Romeiro <leozinwork@gmail.com>
2024-01-26 09:38:05 -0300

    Testando Handler

--- PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -1,4 +1,7 @@
+using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Enums;
 using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
 
 namespace PaymentContext.Tests.Handlers;
 
@@ -6,6 +9,36 @@ namespace PaymentContext.Tests.Handlers;
 public class SubscriptionHandlerTests{
     [TestMethod]
     public void ShouldReturnErrorWhenDocumentExists(){
-        var handler =  new SubscriptionHandler();
+        var handler =  new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailService());
+        var command = new CreateBoletoSubscriptionCommand();
+        
+        command.FirstName="Bruce";
+        command.LastName="Wayne";
+        command.NumberDocument="99999999999";
+        command.AddressEmail="hello@balta.io";
+
+        command.BarCode="123456";
+        command.BoletoNumber="123456";
+
+        command.PaymentNumber="123456";
+        command.PaidDate=DateTime.Now;
+        command.ExpireDate=DateTime.Now.AddMonths(1);
+        command.Total=60;
+        command.TotalPad=60;
+        command.Payer="WAYNE CORP";
+        command.PayerDocument="12345678911";
+        command.PayerEmail="CORP@dc.com";
+        command.PayerDocumentType = EDocumentType.CPF;
+
+        command.Street="Rua";
+        command.Number="41";
+        command.Neighborhood="teste";
+        command.City="Cidade";
+        command.State="Estado";
+        command.Country="conta";
+        command.ZipCode="CEP";
+
+        handler.Handle(command);
+        Assert.AreEqual(false,handler.IsValid);
     }
 }
