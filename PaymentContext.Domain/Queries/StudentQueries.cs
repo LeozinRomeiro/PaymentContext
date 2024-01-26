743f413384b37c35da2ea3c24bd9615cbb5a8ae3
Leonardo Romeiro <leozinwork@gmail.com>
2024-01-26 09:49:15 -0300

    Queries

---
+++ PaymentContext.Domain/Queries/StudentQueries.cs
@@ -0,0 +1,11 @@
+using System.Linq.Expressions;
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Queries;
+
+public static class StudentQueries
+{
+    public static Expression<Func<Student, bool>> GetStudent(string document){
+        return x => x.Document.Number == document;
+    }
+}
